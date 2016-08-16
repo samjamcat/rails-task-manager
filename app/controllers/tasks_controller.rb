1848043f244b99c2c9d0ecd272d631f4840637ec
sam cathcart <sc@10past12.co>
2016-08-16 13:54:29 +0100

    add controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,31 +1,40 @@
 class TasksController < ApplicationController
-  @tasks = Task.all
+
 
   def index
-
+    @task = Task.all
   end
 
   def show
-
+    @task = Task.find(params[:id])
   end
 
   def new
-
+    @task = Task.New
   end
 
   def create
-
+    Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def edit
+    @task = Task.find(params[:id])
 
   end
 
   def update
+    @task = Task.find(params[:id])
+    Task.update(task_params)
+    redirect_to tasks_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
+  end
 
+  def set_restaurant
+      ????
   end
 
 end
