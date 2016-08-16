d35a6070684d2992fb4bbcd5742bc783114da51f
sam cathcart <sc@10past12.co>
2016-08-16 16:03:28 +0100

    all finished ex 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,40 +1,47 @@
 class TasksController < ApplicationController
 
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
 
   def index
-    @task = Task.all
+    @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
-    @task = Task.New
+    @task = Task.new
   end
 
+
   def create
-    Task.create(task_params)
+    @task = Task.create(task_params)
     redirect_to tasks_path
   end
 
   def edit
-    @task = Task.find(params[:id])
-
   end
 
   def update
-    @task = Task.find(params[:id])
-    Task.update(task_params)
+    @task.update(task_params)
     redirect_to tasks_path
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
-  def set_restaurant
-      ????
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: "read book", completed: false)
+Task.create(name: "dry cleaning", completed: false)
+Task.create(name: "tidy garden", completed: false)
